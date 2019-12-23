48e184cccddda4c130e860033d24c63c23e4a127
Kyle1413 <kylet1412@gmail.com>
2019-12-22 21:21:19 -0500

    remove unused config values

--- BeatSaverDownloader/Misc/PluginConfig.cs
+++ BeatSaverDownloader/Misc/PluginConfig.cs
@@ -6,9 +6,9 @@ namespace BeatSaverDownloader.Misc
     {
         static private BS_Utils.Utilities.Config config = new BS_Utils.Utilities.Config("BeatSaverDownloader");
 
-        public static string beatsaverURL = "https://beatsaver.com";
+      //  public static string beatsaverURL = "https://beatsaver.com";
 
-        public static string scoresaberURL = "https://scoresaber.com";
+      //  public static string scoresaberURL = "https://scoresaber.com";
 
         public static int maxSimultaneousDownloads = 3;
 
@@ -24,15 +24,15 @@ namespace BeatSaverDownloader.Misc
 
         public static void Load()
         {
-            beatsaverURL = config.GetString("BeatSaverDownloader", "beatsaverURL", "https://beatsaver.com", true);
-            scoresaberURL = config.GetString("BeatSaverDownloader", "scoresaberURL", "https://scoresaber.com", true);
+      //      beatsaverURL = config.GetString("BeatSaverDownloader", "beatsaverURL", "https://beatsaver.com", true);
+       //     scoresaberURL = config.GetString("BeatSaverDownloader", "scoresaberURL", "https://scoresaber.com", true);
             maxSimultaneousDownloads = config.GetInt("BeatSaverDownloader", "maxSimultaneousDownloads", 3, true);
         }
 
         public static void SaveConfig()
         {
-            config.SetString("BeatSaverDownloader", "beatsaverURL", beatsaverURL);
-            config.SetString("BeatSaverDownloader", "scoresaberURL", scoresaberURL);
+       //     config.SetString("BeatSaverDownloader", "beatsaverURL", beatsaverURL);
+       //     config.SetString("BeatSaverDownloader", "scoresaberURL", scoresaberURL);
             config.SetInt("BeatSaverDownloader", "maxSimultaneousDownloads", maxSimultaneousDownloads);
         }
     }
