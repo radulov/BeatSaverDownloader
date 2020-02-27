e464d48fe168067dea456a6277e37cd4eca98128
Kyle1413 <kylet1412@gmail.com>
2020-02-27 17:05:44 -0500

    Update PluginUI.cs

--- BeatSaverDownloader/UI/PluginUI.cs
+++ BeatSaverDownloader/UI/PluginUI.cs
@@ -48,7 +48,7 @@ namespace BeatSaverDownloader.UI
         {
             if (_moreSongsFlowCooridinator == null)
                 _moreSongsFlowCooridinator = BeatSaberUI.CreateFlowCoordinator<MoreSongsFlowCoordinator>();
-            _moreSongsFlowCooridinator.SetParentFlowCoordinator(BeatSaberMarkupLanguage.BeatSaberUI.MainFlowCoordinator, false);
+            _moreSongsFlowCooridinator.SetParentFlowCoordinator(BeatSaberMarkupLanguage.BeatSaberUI.MainFlowCoordinator, true);
             BeatSaberMarkupLanguage.BeatSaberUI.MainFlowCoordinator.PresentFlowCoordinator(_moreSongsFlowCooridinator); // ("PresentFlowCoordinator", _moreSongsFlowCooridinator, null, false, false);
         }
     }
